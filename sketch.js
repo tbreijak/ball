e7e00513621e1da3fb35aa15115639e4a9689c1b
tbreijak <ab6131@wayne.edu>
2016-02-11 16:52:01 -0500

    copied files
    copied files

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 var ball = {
-  x: 20,
+  x: 200,
   y: 200,
   size: 15,
+  directionX: 2,
+  directionY: -4,
+
 
   display: function () {
     // note that to access any of ball's properties, you must use dot notation, using the special keyword this
@@ -14,18 +17,27 @@ var ball = {
   },
 
   move: function () {
-    this.x += 1; // this line is the same as: this.x = this.x + 1;
+    this.y += this.directionY;
+    this.x += this.directionX;// this line is the same as: this.x = this.x + 1;
   },
 
   checkForBounce: function () {
-    if (this.x > width - this.size / 2) this.bounce();
-    if (this.x < 0 + this.size / 2) this.bounce();
+    if (this.y > height - this.size / 2) this.bounceY();
+    if (this.y < 0 + this.size / 2) this.bounceY();
+    if (this.x > width - this.size / 2) this.bounceX();
+    if (this.x < 0 + this.size / 2) this.bounceX();
   },
 
-  bounce: function () {
+  bounceY: function () {
     // your code goes here; a hint: this will require making your own variable as well as defining this function
     // another hint: notice that ball is defined using object literal notation
-  }
+    this.directionY = this.directionY*-1;
+  },
+
+
+  bounceX: function() {
+    this.directionX = this.directionX*-1;
+},
 };
 
 var setup = function() { // setup is called only when the sketch starts
